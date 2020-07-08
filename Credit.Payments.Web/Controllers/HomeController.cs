4c3a5ebb9a06a700bebde93017e0b297f90770b5
Fernando Méndez <mendezc.jf@gmail.com>
2020-07-07 20:31:52 -0500

    Adding logic to create table of historic payments

--- Credit.Payments.Web/Controllers/HomeController.cs
+++ Credit.Payments.Web/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Credit.Payments.Web.Models;
+using System.Collections;
 
 namespace Credit.Payments.Web.Controllers
 {
@@ -20,7 +18,35 @@ namespace Credit.Payments.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            int maxYears = 3;
+            Hashtable years = new Hashtable();
+            DateTime date = new DateTime(2018, 10, 30);
+            string history = "111111111111111111111111";
+
+            for (int i = 0; i < history.Length; i++)
+            {
+                // If we don't have the key/row for the year
+                if (!years.ContainsKey(date.Year))
+                {
+                    // And we haven't reach the 'maxYears'
+                    if (years.Count == maxYears)
+                    {
+                        break;
+                    }
+
+                    // Then add the year to the hastable with an array to hold the months
+                    years.Add(date.Year, new char[12]);
+                }
+
+                // Here we get the months of the year
+                char[] months = (char[])years[date.Year];
+                // Set the value of history[i] for the current month in array
+                months[date.Month - 1] = history[i];
+                // Move one month to the past
+                date = date.AddMonths(-1);
+            }
+
+            return View(years);
         }
 
         public IActionResult Privacy()
